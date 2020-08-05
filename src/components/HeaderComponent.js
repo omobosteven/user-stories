b4d97a9b88c9045e2ca99b4142681d87b0ea1a41
omobosteven <omobosteven@gmail.com>
2020-08-05 11:50:15 +0100

    ch: reconfigure-setup

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,48 +4,47 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'antd';
 import doLogOutUser from '../actions/logOut';
-import '../styles/header.css';
+import '../styles/header.scss';
 
-const HeaderComponent = ({
-  isLoggedIn, isAdmin, user, onLogOutUser,
-}) => {
-  const handleLogOut = () => {
-    onLogOutUser();
-  };
+const HeaderComponent = ({ isLoggedIn, isAdmin, user, onLogOutUser }) => {
+	const handleLogOut = () => {
+		onLogOutUser();
+	};
 
-  return (
-    <header className="main-header">
-      <h1 className="main-header__title">
-        UserStory
-        <span className="main-header__username">
-          {isLoggedIn && `${user.firstName} ${user.lastName}`}
-        </span>
-      </h1>
-      {isLoggedIn
-      && <div className="main-header__navigation">
-        {!isAdmin && <NavLink to="/create-story">Create Story</NavLink>}
-        <NavLink to="/stories">View Stories</NavLink>
-        <Button className="logout-btn" onClick={() => handleLogOut()}>Logout</Button>
-        </div>}
-    </header>
-  );
+	return (
+		<header className="main-header">
+			<h1 className="main-header__title">
+				UserStory
+				<span className="main-header__username">{isLoggedIn && `${user.firstName} ${user.lastName}`}</span>
+			</h1>
+			{isLoggedIn && (
+				<div className="main-header__navigation">
+					{!isAdmin && <NavLink to="/create-story">Create Story</NavLink>}
+					<NavLink to="/stories">View Stories</NavLink>
+					<Button className="logout-btn" onClick={() => handleLogOut()}>
+						Logout
+					</Button>
+				</div>
+			)}
+		</header>
+	);
 };
 
 HeaderComponent.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
-  isAdmin: PropTypes.bool.isRequired,
-  user: PropTypes.objectOf(PropTypes.any).isRequired,
-  onLogOutUser: PropTypes.func.isRequired,
+	isLoggedIn: PropTypes.bool.isRequired,
+	isAdmin: PropTypes.bool.isRequired,
+	user: PropTypes.objectOf(PropTypes.any).isRequired,
+	onLogOutUser: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  isAdmin: state.userState.isAdmin,
-  isLoggedIn: state.userState.isLoggedIn,
-  user: state.userState.user,
+	isAdmin: state.userState.isAdmin,
+	isLoggedIn: state.userState.isLoggedIn,
+	user: state.userState.user,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  onLogOutUser: () => dispatch(doLogOutUser()),
+	onLogOutUser: () => dispatch(doLogOutUser()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderComponent);
